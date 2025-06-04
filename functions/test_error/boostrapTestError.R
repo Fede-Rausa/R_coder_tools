d4281cbf7b9b6cdaa2d1763cc8725047640bf4de
Fede-Rausa <126892947+Fede-Rausa@users.noreply.github.com>
2025-06-05 00:53:21 +0200

    Create boostrapTestError.R

---
+++ functions/test_error/boostrapTestError.R
@@ -0,0 +1,31 @@
+
+
+library(caret)
+
+bootstrap_error = function(df, yname, error_fun, model_fun, 
+                      niter=3, tr_prp = 0.7, seed=42,
+                      params=list()){
+  
+  
+  testerror = numeric(niter)
+  models = list()
+  
+  for (i in 1:niter){
+    tts = train_test_split(df=df, tr_prp=tr_prp, yname=yname, seed=i+seed)
+    
+    models[[i]] = model_fun(tts$train, tts$test, 
+                            yname=yname, error_fun=error_fun, 
+                            params=params)
+    
+    testerror[i] = models[[i]]$test_error
+  }
+  
+  
+  return(list(bterror=testerror,
+              meanerror = mean(testerror),
+              models = models))
+}
+
+
+
+
