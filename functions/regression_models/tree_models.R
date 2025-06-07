c5480a53ccf567f9ea5c641115111fbfc03b8a6b
Fede-Rausa <126892947+Fede-Rausa@users.noreply.github.com>
2025-06-07 13:49:52 +0200

    Update tree_models.R

--- functions/regression_models/tree_models.R
+++ functions/regression_models/tree_models.R
@@ -1,20 +1,18 @@
 
 
-########## rpart CART tree -------------
+######### rpart regression tree ----------------------------
 
-library(rpart)
 
-model_tree = function(train, test, yname, 
-                     error_fun, params=list(
-                       method='anova', minbucket=20,
-                       cp=0.01, xval=1
-                     )){
+model_rtree = function(train, test, yname, 
+                      error_fun, params=list(
+                        minbucket=20,
+                        cp=0.01, xval=1
+                      )){
   
   N = nrow(train)
+  library(rpart)
   
-  if(is.null(params$method)){
-    params$method='anova'
-  }
+
   if(is.null(params$minsplit)){
     params$minsplit=20
   }
@@ -28,15 +26,9 @@ model_tree = function(train, test, yname,
     params$xval=1
   }
   
-  # if (params$method == 'class'){
-  #   train[,yname] = factor(train[,yname], 
-  #                           levels=unique(train[,yname]))
-  #   test[,yname] = factor(test[,yname], 
-  #                         levels=levels(train[,yname]))
-  # }
-  
   f = formula(paste0(yname, '~.'))
-  model = rpart(f, data = train, method=params$method, weights=params$w,
+  model = rpart(f, data = train, method='anova', 
+                weights=params$w,
                 control = rpart.control(
                   minsplit = params$minsplit,
                   minbucket = params$minbucket,
@@ -44,11 +36,7 @@ model_tree = function(train, test, yname,
                   xval=params$xval))
   
   pred = function(df){
-    if (params$method=='anova'){
-      preds = predict(model, df)
-    }else{
-      preds = predict(model, df, type='class')
-    }
+    preds = predict(model, df)
     return(preds)
   }
   
@@ -58,40 +46,22 @@ model_tree = function(train, test, yname,
   train_error = error_fun(train_preds, train[,yname])
   test_error = error_fun(test_preds, test[,yname])
   
-  if (params$method=='anova'){
-    train_residuals = train_preds - train[,yname]
-    test_residuals = test_preds - test[,yname]
-  }else{
-    train_residuals = NULL
-    test_residuals = NULL
-  }
 
-  
-  if (params$method=='anova'){
-    id0 = round(train_preds)
-  }else{
-    id0 = train_preds
-  }
+  train_residuals = train_preds - train[,yname]
+  test_residuals = test_preds - test[,yname]
+  id0 = round(train_preds)
+  id0[id0<0] = 1
   id1 = unique(id0)
   dict = rank(id1)
   names(dict) = id1
   
   
   pred_clu = function(df){
-    if (params$method=='anova'){
-      indexes = as.character(round(pred(df)))
-    }else{
-      indexes = as.character(pred(df))
-    }
+    indexes = as.character(round(pred(df)))
     clusters = dict[indexes]
     return(clusters)
   }
   
-  if (params$method=='anova'){
-    id0[id0<0] = 1
-  }
-  
-
   train_clu = dict[id0]
   test_clu = pred_clu(test)
   
@@ -105,6 +75,8 @@ model_tree = function(train, test, yname,
               train_residuals=train_residuals,
               test_residuals=test_residuals))
 }
+
+
 
 
 ########## rf ------------
